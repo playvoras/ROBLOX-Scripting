fb96ff7a17df153411e9c383d2278d88d972014f
playvora <longliator.dev@gmail.com>
2024-10-09 14:03:22 +0700

    Update TheRedLake.lua

--- TheRedLake.lua
+++ TheRedLake.lua
@@ -133,12 +133,8 @@ section1:NewToggle("Enemy ESP", "", function(o)
     end 
 end)
 
-section1:NewButton("go to f1 loot room", "", function()
-    plr.Character.HumanoidRootPart.CFrame = CFrame.new(Vector3.new(1329, -233, -171))
-end)
-
-section1:NewButton("go to gold loot room", "", function()
-    plr.Character.HumanoidRootPart.CFrame = CFrame.new(Vector3.new(753, -233, -755))
+section1:NewButton("go to scp 079 room", "", function()
+    plr.Character.HumanoidRootPart.CFrame = CFrame.new(Vector3.new(850, -233, -518))
 end)
 
 for i, v in ipairs(workspace:GetDescendants()) do
