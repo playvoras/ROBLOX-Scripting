380ef51b67ae4b08f765acd5135025f3e33f63fe
playvora <longliator.dev@gmail.com>
2024-10-12 14:34:48 +0700

    Update TheRedLake.lua

--- TheRedLake.lua
+++ TheRedLake.lua
@@ -178,11 +178,11 @@ end)
 local junkFolder = workspace["Bullets/Junk"]
 if junkFolder then
     for _, child in pairs(junkFolder:GetChildren()) do 
-        wait(1)
+        task.wait()
         child:Destroy() 
     end
     junkFolder.ChildAdded:Connect(function(child)
-        wait(1)
+        task.wait()
         child:Destroy()
     end)
 end
